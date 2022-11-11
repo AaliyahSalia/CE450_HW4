52dc9a08c3cb391e6e83db4150c98a785041e18b
AaliyahSalia <101353473+AaliyahSalia@users.noreply.github.com>
2022-11-10 17:38:11 -0800

    Update mk_wd.py

--- mk_wd.py
+++ mk_wd.py
@@ -1,5 +1,5 @@
 
-def make_withdraw(balance):
+def mk_wd(balance):
     def withdraw(amount):
         nonlocal balance
         if amount > balance:
@@ -8,8 +8,8 @@ def make_withdraw(balance):
         return balance
     return withdraw
 
-withdraw = make_withdraw(100)
-wd = make_withdraw(100)
+withdraw = mk_wd(100)
+wd = mk_wd(100)
 print(wd(10))
 print(wd(20))
 print(wd(100))
